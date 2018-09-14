7f118d40a5a0d6787c8e3d5c5c12cd4bc953e7ef
Akshay Narayan <okkhoy@gmail.com>
2018-09-14 12:39:09 +0800

    Update university

--- src/com/um/University.java
+++ src/com/um/University.java
@@ -1,4 +1,11 @@
 package com.um;
 
+import com.um.staff.Staff;
+import com.um.student.Student;
+
+import java.util.ArrayList;
+
 public class University {
+    ArrayList<Student> students;
+    ArrayList<Staff> staffList;
 }
